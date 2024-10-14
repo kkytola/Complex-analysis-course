9b290e64de65cdd3e14b13ebe2497f389d906847
kalle <kalle.kytola@aalto.fi>
2024-10-14 21:31:52 +0300

    Setup blueprint

--- lakefile.lean
+++ lakefile.lean
@@ -14,3 +14,9 @@ lean_exe «complexanalysiscourse» where
   -- `runFrontend`) at the expense of increased binary size on Linux.
   -- Remove this line if you do not need such functionality.
   supportInterpreter := true
+
+require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
+
+meta if get_config? env = some "dev" then
+require «doc-gen4» from git
+  "https://github.com/leanprover/doc-gen4" @ "main"
